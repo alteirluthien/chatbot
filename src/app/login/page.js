cbe224db2e30183436f8fe10b9e5acff41bc8aa5
alteirluthien <alteirluthien@gmail.com>
2025-09-08 23:00:24 +1000

    edit

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-import "@/styles/login.css";
+import "@/login/login.css";
 
 export default function Login() {
   const [email, setEmail] = useState('');
