d3e72936cc7d23c521703f71b846c23331021c89
alteirluthien <alteirluthien@gmail.com>
2025-09-14 21:44:53 +1000

    edit

--- src/app/api/list-users/route.js
+++ src/app/api/list-users/route.js
@@ -4,7 +4,7 @@ import clientPromise from '@/lib/mongodb';
 export async function GET() {
   try {
     const client = await clientPromise;
-    const db = client.db('college_enquiry');
+    const db = client.db('chatbot');
     const users = await db.collection('users').find({}).toArray();
     
     // Remove password field for security
